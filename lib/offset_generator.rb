5959f5ca8f1d88a8a65520b274895f5c7760cbe2
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-04 19:30:41 -0600

    all tests working for offset generator

--- lib/offset_generator.rb
+++ lib/offset_generator.rb
@@ -7,12 +7,12 @@ class OffsetGenerator
   def initialize(key= KeyGenerator.new , date= Date.today)
     @key = key
     @key_offsets = []
-    @date = Date.strptime(date, '%Y-%m-%d')
+    @date = date
     @date_offsets = []
   end
 
   def generate_key_offsets
-    key_numbers = @key.split
+    key_numbers = @key.to_s.chars
     key_a = (key_numbers[0] + key_numbers[1]).to_i
     key_b = (key_numbers[1] + key_numbers[2]).to_i
     key_c = (key_numbers[2] + key_numbers[3]).to_i
@@ -20,26 +20,20 @@ class OffsetGenerator
     @key_offsets << key_a << key_b << key_c << key_d
   end
 
-  def get_date
-    date_nums = @date.to_s.split("-")
-    # binding.pry
-    year_nums = date_nums.shift.chars
-    2.times {year_nums.shift}
-    year_nums = year_nums.join
-    day_nums = date_nums.pop
-    @date = date_nums.unshift(day_nums).push(year_nums).join.to_i
+  def format_date
+    @date.strftime("%d%m%y")
   end
 
-  def generate_date_offsets(date)
-    @date_offsets = ((@date ** 2).to_s[-4..-1]).chars
+  def generate_date_offsets
+    @date_offsets = ((format_date.to_i ** 2).to_s[-4..-1]).chars
     @date_offsets = @date_offsets.map {|num| num.to_i}
   end
 
-  def generate_offsets
+  def generate_rotations
     offset_a = @key_offsets[0] + @date_offsets[0]
     offset_b = @key_offsets[1] + @date_offsets[1]
     offset_c = @key_offsets[2] + @date_offsets[2]
     offset_d = @key_offsets[3] + @date_offsets[3]
-    offsets = [offset_a, ]
+    offsets = [offset_a, offset_b, offset_c, offset_d]
   end
 end
--- test/offset_generator_test.rb
+++ test/offset_generator_test.rb
@@ -12,32 +12,29 @@ class OffsetGeneratorTest < Minitest::Test
 
 
   def test_it_can_generate_key_offsets
-    offset = OffsetGenerator.new(12345, 2016-04-03)
+    offset = OffsetGenerator.new(12345)
     assert_equal [12, 23, 34, 45], offset.generate_key_offsets
 
   end
 
   def test_it_can_get_date_into_the_right_format
-    skip 
-    offset = OffsetGenerator.new(01234, 2016-04-04)
-    assert_equal 40416, offset.get_date
+    offset = OffsetGenerator.new(12345, Date.parse("2012-04-03"))
+    assert_equal "030412", offset.format_date
   end
+
+  #edge case: year like 2008 becomes 08 instead of 8 
 
   def test_it_can_generate_date_offsets
-    skip
-    offset = OffsetGenerator.new
-    offset.get_date
-    assert_equal [], offset.generate_date_offsets
-
+    offset = OffsetGenerator.new(12345, Date.parse("2012-04-03"))
+    offset.format_date
+    assert_equal [9, 7, 4, 4], offset.generate_date_offsets
   end
 
-  def test_it_can_create_final_offsets_from_key_and_date_offsets
-    skip
-    offset = OffsetGenerator.new
+  def test_it_can_create_final_rotations_from_key_and_date_offsets
+    offset = OffsetGenerator.new(12345, Date.parse("2012-04-03"))
     offset.generate_key_offsets
-    offset.get_date
     offset.generate_date_offsets
-    assert_equal 12345, offset.generate_offsets
+    assert_equal [21, 30, 38, 49], offset.generate_rotations
   end
 
 end
