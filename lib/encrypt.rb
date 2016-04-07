d208fa7180044cd9e512b8042342e481a18ba606
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-07 08:54:55 -0600

    file IO decrypt and encrypt methods working

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,12 +7,14 @@ offset_generator = OffsetGenerator.new
 handle = File.open(ARGV[0], "r")
 plain_text = handle.read.chomp
 handle.close
-encrypted_text = e.encrypt(plain_text)
+
+key = e.key.generate
+encrypted_text = e.encrypt(plain_text, key)
 
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_text)
 writer.close
 
-key = e.key.generate
+
 date = offset_generator.format_date
 puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
